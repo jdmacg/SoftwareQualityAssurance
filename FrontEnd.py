6854d946c459202890ea0ff6ecea29f25d9fe1b4
Jordan McGregor <11jdam@queensu.ca>
2016-10-20 14:58:13 -0400

    fixed merge

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,10 +1,16 @@
 #the main file of the front end
 from Session import Session
+from Valid
+import sys
+accountsIndex = 1
+transactionIndex = 2
 
+accountsFile = open(sys.argv[accountsIndex])
+transactionFile = open(sys.argv[transactionIndex])
 
 while True:
 	userInput = raw_input("Enter a command : ")
-	session = Session(userInput)
+	session = Session(userInput,accountsFile, transactionFile)
 	if session.isReady is True:
 		break
 
@@ -13,5 +19,4 @@ while True:
 while userInput != "logout":
 	userInput = session.getInput()
 	session.runCommand(userInput)
-
 
