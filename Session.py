418f21e89f246e8a14c1bc2f96e214bec6bbd5ac
NicholasPetrielli <nicholas.petrielli@gmail.com>
2016-10-20 13:44:13 -0400

    Update Session.py

--- Session.py
+++ Session.py
@@ -1,7 +1,7 @@
 class Session:
 	def __init__(self,isAdmin):
 		self.admin = isAdmin
-		
+	
 	def command(self,userInput):
 		if userInput == "create" and self.admin == 1:
 			userInput = input("Enter account number")
@@ -24,12 +24,13 @@ class Session:
 				return Session(1)
 			else:
 				return Session(0)
+	
 	def checkValidAmount(self,amount):
 		if self.admin == 1 and amount < 99999999 and amount > 0 and len(str(amount)) >= 3 and len(str(amount)) <= 8:
 			return 1
 		else:
 			return 0
-		if self.admin == 0 and amount < 1000 and amount > 0:
+		if self.admin == 0 and amount < 1000 and amount > 0 and len(str(amount)) >= 3 and len(str(amount)) <= 8:
 			return 1
 		else:
 			return 0
