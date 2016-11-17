dbfcf2aceec00fa3a917ad576863c395082c9b01
jdmacg <jordanmcgregor@Jordans-MacBook-Pro.local>
2016-11-16 20:12:49 -0500

    Add test
    Test for creating account with amount less than zero

--- Source/BackEnd/TestAccount.py
+++ Source/BackEnd/TestAccount.py
@@ -8,16 +8,28 @@ class TestAccount(unittest.TestCase):
     def test_createAccount_alreadyExistingAccountNumber(self):
         accountNum = "00000000"
         accountAmount = 0
-        accountName = "A"
+        accountName = "ABC"
         accountDict = dict()
 
         existingAccount = Account(accountNum, accountAmount, accountName, accountDict)
         self.assertEqual(1, len(accountDict))
-        numAccounts = 1
+        expectedNumAccounts = 1
 
         copyAccount = Account(accountNum, accountAmount, accountName, accountDict)
-        self.assertEqual(numAccounts, len(accountDict))
+        self.assertEqual(expectedNumAccounts, len(accountDict))
 
 
-    
+    def test_createAccount_amountLessThanZero(self):
+        accountNum = "00000000"
+        accountAmount = -1
+        accountName = "ABC"
+        accountDict = dict()
 
+        expectedNumAccounts = 0
+
+        newAccount = Account(accountNum, accountAmount, accountName, accountDict)
+        self.assertEqual(expectedNumAccounts, len(accountDict))
+
+
+if __name__ == '__main__':
+    unittest.main()
