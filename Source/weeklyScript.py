484d1c8bf55b1f3bc89d1c5b6e0f6df7574192d0
Nicholas Petrielli <nicholas.petrielli@gmail.com>
2016-12-06 22:48:11 -0500

    weekly script

--- Source/weeklyScript.py
+++ Source/weeklyScript.py
@@ -6,17 +6,18 @@ import dailyScript
 textPath = sys.path[0] + '/textInput/'
 weeklyText = sys.path[0] + '/weeklyTextInput/'
 validAccountBackend = sys.path[0] + '/BackEnd/ValidAccounts.txt'
+masterAccountBackend = sys.path[0] + '/BackEnd/MasterAccountList.txt'
 frontEndDir = sys.path[0] + '/FrontEnd/'
-
+masterAccount = sys.path[0] + '/weeklyMasterAccount/'
 
 class weeklyScript:
 	
 	def __init__(self):
 		self.hasRan = 0
 
-	def cleanTextPath(self):
-		textFiles = os.listdir(textPath)
-		textFiles = [textPath + textFile for textFile in textFiles if '.' in textFile]
+	def cleanTextPath(self, directory):
+		textFiles = os.listdir(directory)
+		textFiles = [directory + textFile for textFile in textFiles if '.' in textFile]
 		for textFile in textFiles:
 			os.system('rm ' + textFile)
 
@@ -30,14 +31,19 @@ class weeklyScript:
 		weeklyDirectories = os.listdir(weeklyText)
 		for directory in weeklyDirectories:
 			newFileDir = weeklyText + directory + '/'
-			self.cleanTextPath()
+			self.cleanTextPath(textPath)
 			self.copyFiles(newFileDir)
 			os.chdir(sys.path[0] + '/BackEnd/')
-			dailyScript
+			dailyScript.startNewDay()
 			os.chdir(sys.path[0])
 			os.system('cp ' + validAccountBackend + ' ' + frontEndDir)
+			os.system('cp ' + masterAccountBackend + ' ' + masterAccount)
+			masterAccountName = masterAccount + 'MasterAccountList.txt'
+			newMasterAccountName  = masterAccount + 'MasterAccountList' + str(directory) + '.txt'
+			os.system('mv ' + masterAccountName + ' ' + newMasterAccountName)
 
 
 
 newWeek = weeklyScript()
+newWeek.cleanTextPath(masterAccount)
 newWeek.runWeekly()
