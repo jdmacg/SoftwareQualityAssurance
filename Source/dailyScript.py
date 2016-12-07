1880e79e89fe0e3b38619bafab8b8911fb2b93f8
Nicholas Petrielli <nicholas.petrielli@gmail.com>
2016-12-06 21:06:20 -0500

    Adding new daily script

--- Source/dailyScript.py
+++ Source/dailyScript.py
@@ -7,6 +7,7 @@ frontEndPath = sys.path[0] + '/FrontEnd/FrontEnd.py'
 backEndPath = sys.path[0] + '/BackEnd/BackEnd.py'
 runScriptPath = sys.path[0] + '/RunScript.sh'
 textPath = sys.path[0] + '/textInput/'
+transactionSummPath = sys.path[0] + '/dailyTransactionFiles/'
 
 class dailyScript:
 
@@ -14,6 +15,7 @@ class dailyScript:
 		self.hasRan = 0;
 		self.testOuput = None
 	def runTestFront(self, testFile):
+		#testOutput = os.system('python ' + frontEndPath + ' < ' + testFile)
 		testOutput = subprocess.check_output([runScriptPath,frontEndPath, testFile])
 		return testOutput
 	
@@ -24,10 +26,37 @@ class dailyScript:
 	
 	def runFrontEnd(self):
 		textFiles = self.getTextFiles(textPath)
+		i = 1
 		for textFile in textFiles:
 			textOutput = self.runTestFront(textFile)
-			print textOutput
+			transactionFile = sys.path[0] + '/FrontEnd/TransactionSummary.txt';
+			os.system('cp ' + transactionFile + ' ' + transactionSummPath)
+			oldFileName = transactionSummPath + 'TransactionSummary.txt'
+			newFileName = transactionSummPath + 'TransactionSummary' + str(i) + '.txt'
+			os.system('mv ' + oldFileName + " " + newFileName) 
+			i = i + 1
+	def clearOldTransactionFiles(self):
+		oldTransactions = os.listdir(transactionSummPath)
+		oldTransactions = [transactionSummPath + oldTransaction for oldTransaction in oldTransactions if '.' in oldTransaction]
+		for oldTransaction in oldTransactions:
+			os.system('rm ' + oldTransaction)
+	
+	def mergeTransactionFiles(self):
+		transFiles = os.listdir(transactionSummPath)
+		transFiles = [transactionSummPath + transFile for transFile in transFiles if '.' in transFile]
+		mergedFileLocation = sys.path[0] + '/BackEnd/MergedTransactionSummaryFile.txt';
+		merging = open(mergedFileLocation, 'w+')
+		for transFile in transFiles:
+			f = open(transFile, 'r')
+			for line in f:
+				merging.write(line)
 
+	def callBackEnd(self):
+		os.chdir(sys.path[0] + '/BackEnd/')
+		os.system('python ' + backEndPath)	
 
 newDay = dailyScript()
+newDay.clearOldTransactionFiles()
 newDay.runFrontEnd()
+newDay.mergeTransactionFiles()
+newDay.callBackEnd()
