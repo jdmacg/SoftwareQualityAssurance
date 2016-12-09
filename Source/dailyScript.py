ba71b81e491c2879940ee3d9c89cfbd953bbb6c3
Nicholas Petrielli <nicholas.petrielli@gmail.com>
2016-12-09 12:37:44 -0500

    commented files

--- Source/dailyScript.py
+++ Source/dailyScript.py
@@ -1,14 +1,18 @@
 import os 
 import sys
-#sys.path.append(sys.path[0]+'/FrontEnd/')
 import subprocess
 
+#Setting up constant paths based off of system directory
 frontEndPath = sys.path[0] + '/FrontEnd/FrontEnd.py'
 backEndPath = sys.path[0] + '/BackEnd/BackEnd.py'
 runScriptPath = sys.path[0] + '/RunScript.sh'
 textPath = sys.path[0] + '/textInput/'
 transactionSummPath = sys.path[0] + '/dailyTransactionFiles/'
-
+#Inputs:none
+#Outputs:none
+#Description: Instantiates a new dailyScript object, clears all old transaction files
+#Runs the front end with all text inputs in the folder "/textInput/", merges all transaction files
+#And then lastly calls the backend
 def startNewDay():
 	newDay = dailyScript()
 	newDay.clearOldTransactionFiles()
@@ -16,21 +20,40 @@ def startNewDay():
 	newDay.mergeTransactionFiles()
 	newDay.callBackEnd()
 
+
+#Contains all of the necessary classes to run the daily script
 class dailyScript:
 
+	#Initializes a dailyScript class, has to be here because it's a python class
+	#but the actual method does nothing
 	def __init__(self):
 		self.hasRan = 0;
 		self.testOuput = None
+
+	#Input:The name of a text file to run combined with it's path
+	#output:The console output of the python run
+	#Description: Runs a bash script that pipelines the testFile into the python program
+	#The bash is simply: python frontEndPath < testFile
 	def runTestFront(self, testFile):
 		#testOutput = os.system('python ' + frontEndPath + ' < ' + testFile)
 		testOutput = subprocess.check_output([runScriptPath,frontEndPath, testFile])
 		return testOutput
 	
+	#input: Path of where to get test files
+	#output an array consisting of all test files
+	#Description: Finds all files that have a "." in that name in the directory given
 	def getTextFiles(self, path):
 		textFiles = os.listdir(path)
 		textFiles = [path + textFile for textFile in textFiles	if '.' in textFile]
 		return textFiles
 	
+
+	#input: none
+	#output: none
+	#Description: Runs all of the test files with the methods described above
+	#Then it copies the transaction file to a different location and concatanates the string
+	#name with a number to tell the difference between between the transaction files
+	#this is all done using python system commands, and hence will not work if this is not ran on a linux machine
 	def runFrontEnd(self):
 		textFiles = self.getTextFiles(textPath)
 		i = 1
@@ -42,12 +65,20 @@ class dailyScript:
 			newFileName = transactionSummPath + 'TransactionSummary' + str(i) + '.txt'
 			os.system('mv ' + oldFileName + " " + newFileName) 
 			i = i + 1
+
+	#input: none
+	#output: none
+	#Description: clears all transaction files from the location where transactionfiles are saved for the daily script
+	#This is called before the actual textInputs are tested
 	def clearOldTransactionFiles(self):
 		oldTransactions = os.listdir(transactionSummPath)
 		oldTransactions = [transactionSummPath + oldTransaction for oldTransaction in oldTransactions if '.' in oldTransaction]
 		for oldTransaction in oldTransactions:
 			os.system('rm ' + oldTransaction)
 	
+	#Input: none
+	#Output: none
+	#Description: Takes all transaction files created in runFrontEnd() and writes them to the backEnd merged transaction summary file
 	def mergeTransactionFiles(self):
 		transFiles = os.listdir(transactionSummPath)
 		transFiles = [transactionSummPath + transFile for transFile in transFiles if '.' in transFile]
@@ -57,7 +88,9 @@ class dailyScript:
 			f = open(transFile, 'r')
 			for line in f:
 				merging.write(line)
-
+	#Input: none
+	#output: none
+	#Description: Calls the backend using python system, but changes the directory beforehand
 	def callBackEnd(self):
 		os.chdir(sys.path[0] + '/BackEnd/')
 		os.system('python ' + backEndPath)	
--- Source/weeklyScript.py
+++ Source/weeklyScript.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 import dailyScript
 
+#Creating path names based off of the system
 textPath = sys.path[0] + '/textInput/'
 weeklyText = sys.path[0] + '/weeklyTextInput/'
 validAccountBackend = sys.path[0] + '/BackEnd/ValidAccounts.txt'
@@ -10,23 +11,37 @@ masterAccountBackend = sys.path[0] + '/BackEnd/MasterAccountList.txt'
 frontEndDir = sys.path[0] + '/FrontEnd/'
 masterAccount = sys.path[0] + '/weeklyMasterAccount/'
 
+#Contains all of the necessary classes to run the weekly script
 class weeklyScript:
 	
+	#Initialization method for the python class, doesn't really do anything
 	def __init__(self):
 		self.hasRan = 0
 
+	#input: Directory to remove files from
+	#output: none
+	#description:Clears all files in a given directory
+	#this is called to remove all textFiles between daily runs
 	def cleanTextPath(self, directory):
 		textFiles = os.listdir(directory)
 		textFiles = [directory + textFile for textFile in textFiles if '.' in textFile]
 		for textFile in textFiles:
 			os.system('rm ' + textFile)
-
+	
+	#input: directory to copy files from
+	#output: none
+	#description: Copies all files from the given directory to the textInput directory for the dailyScript run 
 	def copyFiles(self, directory):
 		copyFiles = os.listdir(directory)
 		copyFiles = [directory + copyFile for copyFile in copyFiles if '.' in copyFile]
 		for copyFile in copyFiles:
 			os.system('cp ' + copyFile + ' ' + textPath)	
 
+	#Input: none
+	#Output: none
+	#Description: Gets all directories inside the weeklyTextInputs/ folder
+	#and then one directory at a time the files are copied into the textInput, the dailyScript is run
+	#the master account file is saved, and the validAccounts file is transferred to the front end
 	def runWeekly(self):
 		weeklyDirectories = os.listdir(weeklyText)
 		for directory in weeklyDirectories:
